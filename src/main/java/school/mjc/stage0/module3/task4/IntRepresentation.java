9846b9ff8ed03b8b5e9ea93db8278bda4501fe0c
ElenaGeorgievska <elena_science@yahoo.com>
2023-07-27 22:21:49 +0200

    stage0 module 3 task4 completed

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+
+        char c = 99;
+        char h = 104;
+        char a = 97;
+        char r = 114;
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
+
+        //Print chars :'c', 'h', 'a', 'r' using int representation.
+        // Implement program in the following code snippet:
+        //https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html
     }
 }
